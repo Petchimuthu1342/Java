3de5fa1fb17b3e227b6a899b8878958c05135fff
Petchimuthu1342 <petchimuthu1342@gmail.com>
2025-03-20 09:44:33 +0530

    Update NaturalNum.java

--- NaturalNum.java
+++ NaturalNum.java
@@ -6,13 +6,17 @@ public class NaturalNum {
 			Scanner sc=new Scanner(System.in);
 		System.out.println("Get the Number:");
 		int num=sc.nextInt();
-		int rev=0;
+		int i=1;
+		int sum=0;
 		System.out.println("Reverse Number:");
-		while(num!=0){
-		    int rem=num%10;
-			rev=rev*10+rem;
-			num/=10;
-			System.out.print(rem);
+		while(i<=num){
+			sum =num*(num+1)/2;
+			i++;
 		}
-		}
+		System.out.println("Natural Number:"+ sum);
+	        //StringBuffer class
+			StringBuffer sb=new StringBuffer(String.valueOf(sum));
+			StringBuffer rev=sb.reverse();
+			System.out.println("Reverse Number:"+ rev);
+	}
 	}
